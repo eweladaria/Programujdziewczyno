c51f8481265c7a255de4898b38291b6fb2e70361
ewelinadarialukaszewicz@gmail.com <ewelinalukaszewicz@MacBook-Air-Ewelina.local>
2017-10-21 15:58:47 +0200

    Teoria z JavaScript

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var myName='Ewelina';
+var friends=['Asia','Kasia','Judzia'];
+var a=24;
+var b=11;
+if (a < b){
+console.log('hurra');
+  }
+else {
+  console.log('hehe');
+  }
+for (var i = 0; i < friends.length; i++) {
+  console.log(friends[i])
+}
